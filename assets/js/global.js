febb882e70c8c00320831b46dcb0c4a7b960e44a
Mateus santos <msjt098@gmail.com>
2024-04-15 21:00:22 -0300

    Animação topo criada

--- assets/js/global.js
+++ assets/js/global.js
@@ -14,23 +14,60 @@ const animacaoTopo = () => {
            let ball1 = document.getElementById('ball-1')
            let ball2 = document.getElementById('ball-2')
            let ball3 = document.getElementById('ball-3')
-           ball1.remove()
-           ball2.remove()
-           ball3.remove()
+          
+          
+           //esferas que ficam ao redor do logo angular
+           ball1.innerHTML = ''
+           ball2.innerHTML = ''
+           ball3.innerHTML  = ''
+
+           ball1.id = ''
+           ball2.id = ''
+           ball3.id = ''
+
+           //icones do front end
+           ball1.innerHTML = '<i class="fa-brands fa-html5" id="icon1"></i>'
+           ball2.innerHTML = '<i class="fa-brands fa-css3-alt" id="icon2"></i>'
+           ball3.innerHTML = '<i class="fa-brands fa-js" id="icon3"></i>'
+
+            
+
+            
+
+           
+
 
            setTimeout(() => {
             
               h1.remove()
+              ball1.remove()
+              ball2.remove()
+              ball3.remove()
 
+              
+              
+
+            
             
            }, 3000);
 
            
+           setTimeout (() => {
+            let circulo = document.createElement('div')
+            header.appendChild(circulo);
+            circulo.id = 'circulo'
+
+            setTimeout (() => {
+               circulo.remove()
+
+            }, 10000)
+            
+              
+          }, 3000)
+          
 
            
-            
-
-                
+         
         
         
     }, 5000);
